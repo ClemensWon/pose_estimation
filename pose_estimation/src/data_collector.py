d0d86a0c07d4a04ed9c9ee2cffd9cd76f63efa05
ClemensWon <clem1719@gmail.com>
2024-12-28 09:37:55 +0100

    turned topics to services, added color to spawn objects, changed spawn objects, added new launch file, added gitignore,

--- pose_estimation/src/data_collector.py
+++ pose_estimation/src/data_collector.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python3
 
 import os
-import rospy
-import random
 import json
+import time
+
+import rospy
 import rospkg
-from geometry_msgs.msg import Pose
-from std_msgs.msg import Empty, String
-from pose_estimation.msg import ObjectSpawned, DeleteObject
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-from moveit_commander import MoveGroupCommander
+
+from std_srvs.srv import Empty
+
+from pose_estimation.srv import SpawnObject, DeleteObject, MoveJoints, SaveImage
 
 class DataCollector:
     def __init__(self):
         rospy.init_node("data_collector")
 
+        config = rospy.get_param('/generate_dataset')
+        self.iterations = config['iterations']
+        self.robot_poses = config['poses']
+
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        
+        time.sleep(5)
+        self.unpause()
         # Initialize publishers and subscribers
-        self.spawn_trigger_pub = rospy.Publisher("spawn_trigger", Empty, queue_size=10)
-        self.object_spawned_sub = rospy.Subscriber("object_spawned", ObjectSpawned, queue_size=10)
-        self.delete_object_pub = rospy.Publisher("delete_object", DeleteObject, queue_size=10)
-        self.take_picture_pub = rospy.Publisher("/take_picture", String, queue_size=10)
+        rospy.wait_for_service('spawn_object')
+        rospy.wait_for_service('delete_object')
+        rospy.wait_for_service('move_joints')
+        rospy.wait_for_service('save_image')
+        
 
-        # Initialize MoveGroup for robot control
-        self.move_group = MoveGroupCommander("manipulator")
-
-        # Initialize CvBridge for image processing
-        self.bridge = CvBridge()
+        self.spawn_service = rospy.ServiceProxy('spawn_object', SpawnObject)  
+        self.move_joints = rospy.ServiceProxy('move_joints', MoveJoints)
+        self.delete_service = rospy.ServiceProxy('delete_object', DeleteObject)
+        self.save_image_service = rospy.ServiceProxy('save_image', SaveImage)
+         
+        
+        
 
         # Get package path and set dataset folder
         rospack = rospkg.RosPack()
@@ -41,41 +52,33 @@ class DataCollector:
         self.dataset = []
         self.current_image_name = None
 
-        self.robot_poses = [
-            [-0.2, -1.57, 0.0, 0.0, 0.0, -0.735],
-            [-1.67, 0.0, 0.0, -3.14, 2.2, 0.0],
-            [1.57, 0.0, 0.0, -3.14, -2.2, 0.0]
-        ]
 
     def move_robot(self, target_pose):
         rospy.loginfo(f"Moving robot to pose: {target_pose}")
-        self.move_group.set_joint_value_target(target_pose)
-        self.move_group.go(wait=True)
+        response = self.move_joints(target_pose)
 
     def spawn_object(self):
-        rospy.loginfo("Spawning object...")
-        self.spawn_trigger_pub.publish(Empty())
-
-        try:
-            spawn_msg = rospy.wait_for_message("/object_spawned", ObjectSpawned, timeout=10)
-            rospy.loginfo(
-                f"Object spawned: ID={spawn_msg.object_id}, Type={spawn_msg.object_type}, Position=({spawn_msg.pose.position.x}, {spawn_msg.pose.position.y}, {spawn_msg.pose.position.z})")
-            return spawn_msg
-        except rospy.ROSException:
-            rospy.logerr("Timed out waiting for object to spawn.")
-            return None
+        response = self.spawn_service()
+        if response.success:
+            print(f"Spawned object with ID: {response.object_id}")
+            return response
+        else:
+            print(f"Failed to spawn object: {response.message}")
 
     def delete_object(self, object_id):
-        rospy.loginfo(f"Deleting object with ID: {object_id}")
-        delete_msg = DeleteObject()
-        delete_msg.object_id = object_id
-        self.delete_object_pub.publish(delete_msg)
+        response = self.delete_service(object_id=object_id)
+        if response.success:
+            print(f"Deleted object with ID: {object_id}")
+        else:
+            print(f"Failed to spawn object: {response.message}")
+
+        
 
     def take_picture(self, image_name):
         rospy.loginfo(f"Capturing image: {image_name}")
         self.current_image_name = image_name
-        self.take_picture_pub.publish(image_name)
-        rospy.sleep(2)
+        response = self.save_image_service(image_name)
+        return response
 
     def save_data(self):
         dataset_file = os.path.join(self.dataset_folder, "dataset.json")
@@ -83,7 +86,7 @@ class DataCollector:
             json.dump(self.dataset, f, indent=4)
         rospy.loginfo(f"Dataset saved to {dataset_file}")
 
-    def save_transformation_data(self, image_name, object_id, spawn_msg):
+    def save_transformation_data(self, image_name, object_id, spawn_msg, robot_poses):
         data_entry = {
             "image_name": image_name,
             "object_id": object_id,
@@ -98,14 +101,16 @@ class DataCollector:
                 "y": spawn_msg.pose.orientation.y,
                 "z": spawn_msg.pose.orientation.z,
                 "w": spawn_msg.pose.orientation.w
-            }
+            },
+            "robot_pose": robot_poses
         }
         self.dataset.append(data_entry)
+        self.save_data()
 
     def run(self):
         try:
             rospy.loginfo("Starting data collection...")
-            for n in range(2):
+            for n in range(self.iterations):
                 # Spawn object
                 spawn_msg = self.spawn_object()
 
@@ -126,13 +131,11 @@ class DataCollector:
                     self.take_picture(image_name)
 
                     # Save data
-                    self.save_transformation_data(image_name, object_id, spawn_msg)
+                    self.save_transformation_data(image_name, object_id, spawn_msg, self.robot_poses[i])
 
                 # Delete the spawned object
                 self.delete_object(object_id)
 
-            # Save all collected data to JSON
-            self.save_data()
             rospy.loginfo("Data collection complete.")
         except rospy.ROSInterruptException:
             rospy.loginfo("Interrupted before completion.")

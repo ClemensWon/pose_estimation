27c0aaf40a64fd55aab7da839a88826609a34706
Tim Konrad <tim.konrad@gmx.at>
2025-01-08 16:43:00 +0100

    try efficientnet pt2

--- pose_estimation/src/ml/train.py
+++ pose_estimation/src/ml/train.py
@@ -54,7 +54,7 @@ train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=T
 val_loader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False)
 
 # Load Model
-feature_extractor = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+feature_extractor = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
 feature_extractor = nn.Sequential(*list(feature_extractor.children())[:-2])
 model = PoseEstimationModel(feature_extractor, feature_dim=512)
 
